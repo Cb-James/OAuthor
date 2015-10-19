8b1a1edb770ddbcd0375d339d3f406d773a49a12
Cb-James <james@coinbase.com>
2015-10-19 12:51:42 -0700

    add controller actions

--- app/controllers/gate_controller.rb
+++ app/controllers/gate_controller.rb
@@ -1,2 +1,20 @@
+# The 3 steps of Oauth2
 class GateController < ApplicationController
+  def door
+    redirect_to Gatekeeper.greetings
+  end
+
+  def lock
+    response = Gatekeeper.are_you_the_keymaster?(params[:code])
+    session[:access_token] = response['access_token']
+    session[:refresh_token] = response['refresh_token']
+    redirect_to action: 'foyer'
+  end
+
+  def foyer
+    player_one = Gatekeeper.welcomes_you(
+      session[:access_token],
+      session[:refresh_token])
+    @display = player_one.accounts
+  end
 end
